680d62ae4a57c0cdfa15ef12441d044108688ef2
joncrall <jon.crall@kitware.com>
2021-11-04 16:50:22 -0400

    wip

--- kwplot/__main__.py
+++ kwplot/__main__.py
@@ -7,9 +7,13 @@ def main():
     import kwimage
     plt = kwplot.autoplt()
     fpath = sys.argv[1]
+    print('read fpath = {!r}'.format(fpath))
     imdata = kwimage.imread(fpath)
-
+    print('imdata.dtype = {!r}'.format(imdata.dtype))
+    print('imdata.shape = {!r}'.format(imdata.shape))
+    print('normalize')
     imdata = kwimage.normalize_intensity(imdata)
+    print('showing')
     kwplot.imshow(imdata)
 
     plt.show()
