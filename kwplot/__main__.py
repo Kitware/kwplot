97ead13c404d52da516e5b8cab33ee2367596bbe
joncrall <jon.crall@kitware.com>
2021-11-04 16:51:11 -0400

    wip

--- kwplot/__main__.py
+++ kwplot/__main__.py
@@ -4,15 +4,23 @@ def main():
     """
     import kwplot
     import sys
+    import ubelt as ub
     import kwimage
+    import kwarray
     plt = kwplot.autoplt()
     fpath = sys.argv[1]
     print('read fpath = {!r}'.format(fpath))
     imdata = kwimage.imread(fpath)
+
     print('imdata.dtype = {!r}'.format(imdata.dtype))
     print('imdata.shape = {!r}'.format(imdata.shape))
+
+    stats = kwarray.stats_dict(imdata)
+    print('stats = {}'.format(ub.repr2(stats, nl=1)))
+
     print('normalize')
     imdata = kwimage.normalize_intensity(imdata)
+
     print('showing')
     kwplot.imshow(imdata)
 
