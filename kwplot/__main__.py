ba2ac89fd002b446d284640a026fc2e50fcd3265
joncrall <jon.crall@kitware.com>
2022-03-14 13:32:55 -0400

    hack to view a 3rd channel

--- kwplot/__main__.py
+++ kwplot/__main__.py
@@ -11,18 +11,23 @@ def main():
     plt = kwplot.autoplt()
     fpath = sys.argv[1]
     print('read fpath = {!r}'.format(fpath))
-    imdata = kwimage.imread(fpath)
+    imdata = kwimage.imread(fpath, nodata='float')
 
     print('imdata.dtype = {!r}'.format(imdata.dtype))
     print('imdata.shape = {!r}'.format(imdata.shape))
 
-    stats = kwarray.stats_dict(imdata)
+    stats = kwarray.stats_dict(imdata, nan=True)
     print('stats = {}'.format(ub.repr2(stats, nl=1)))
+
+    if kwimage.num_channels(imdata) == 2:
+        import numpy as np
+        # hack for a 3rd channel
+        imdata = np.concatenate([imdata, np.zeros_like(imdata)[..., 0:1]], axis=2)
 
     imdata = kwarray.atleast_nd(imdata, 3)[..., 0:3]
 
     print('normalize')
-    imdata = kwimage.normalize_intensity(imdata, nodata=0)
+    imdata = kwimage.normalize_intensity(imdata)
 
     print('showing')
     from os.path import basename
