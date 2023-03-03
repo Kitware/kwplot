82dfb16d150febd1c73f3df12b6be41c083805d5
joncrall <erotemic@gmail.com>
2023-03-03 15:19:08 -0500

    remove cruft

--- kwplot/mpl_make.py
+++ kwplot/mpl_make.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
 """
 DEPRECATED: use kwimage versions instead
 
 Functions used to explicitly make images as ndarrays using mpl/cv2 utilities
 """
-from __future__ import absolute_import, division, print_function, unicode_literals
 import numpy as np
 from kwimage import make_heatmask, make_vector_field, make_orimask  # NOQA
 
